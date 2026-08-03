da88290dab45d517d57b3812f4b179158925a7fd
venm1r <venmir@gmail.com>
2026-08-04 01:55:00 +0500

    fix(application): remove unused job port import

--- crates/vestrace-application/src/jobs/ports.rs
+++ crates/vestrace-application/src/jobs/ports.rs
@@ -1,6 +1,6 @@
 use crate::ApplicationError;
 use async_trait::async_trait;
-use vestrace_domain::{Job, JobState, id::JobId};
+use vestrace_domain::{Job, id::JobId};
 
 #[async_trait]
 pub trait JobRepository: Send + Sync {
